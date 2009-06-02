a05375c8bcc9fec3394f8276042c7adf710cafd7
Bernd Ustorf <bernd@bernd-ustorfs-macbook-pro.local>
2009-06-02 11:26:28 -0700

    Revert "add default value for start_price of item (0), in order to get test to pass."
    Don't think this is a smart way to get the test to pass.  :)
    
    This reverts commit 17086e52db932791053c4c1e903d3918b14aa266.

--- lib/ebay/types/item.rb
+++ lib/ebay/types/item.rb
@@ -203,11 +203,7 @@ module Ebay # :nodoc:
       object_node :shipping_details, 'ShippingDetails', :class => ShippingDetails, :optional => true
       text_node :ship_to_locations, 'ShipToLocations', :optional => true
       text_node :site, 'Site', :optional => true
-      # needed to add default value for start_price ... bombed in initializer_test b/c the mapper 
-      # does a comparison of the value of the node versus the default.  The == comparison operator
-      # in money.rb complained that it could't find a number of cents for nil, and thus couldn't
-      # do the comparison.  Since the test was whether an error occurred or not, it failed.
-      money_node :start_price, 'StartPrice', :optional => true, :default_value => Money.new(0.0, 'USD')
+      money_node :start_price, 'StartPrice', :optional => true
       object_node :storefront, 'Storefront', :class => Storefront, :optional => true
       text_node :sub_title, 'SubTitle', :optional => true
       text_node :time_left, 'TimeLeft', :optional => true
